299b7ae234b4845d0d4ce66f53ead1ce39a0d7a6
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-01-24 20:47:51 +0100

    Teste Commit

--- StarNative/App.js
+++ StarNative/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to st working on your app!</Text>
+      <Text>Je t'aime mon bebou! <3</Text>
       <StatusBar style="auto" />
     </View>
   );
