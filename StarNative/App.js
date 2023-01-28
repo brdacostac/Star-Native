201f84cf163ce9bfb605c4ef878a12c7ee5f6a1e
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-01-28 23:27:07 +0100

    Icon and Splash added

--- StarNative/App.js
+++ StarNative/App.js
@@ -1,24 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/components/HomeScreen';
 import DetailsScreen from './src/components/DetailsScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import MainTabNavigator from './src/navigation/MainTabNavigator';
+
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const Stack = createNativeStackNavigator();
-
-// let customFonts = {
-//   'Arial': require('./src/assets/fonts/arial.ttf'),
-// }
-// interface iState {
-//   fontsLoaded: boolean;
-// }
-// interface IProps {
-// }
 
 const Tab = createBottomTabNavigator();
 const SettingsStack = createNativeStackNavigator();
@@ -52,29 +39,6 @@ export default function App() {
   );
 }
 
-
-//export default class App extends React.Component<IProps, iState> {
-//   constructor(props: IProps) {
-//     super(props);      
-//     this.state = {
-//         fontsLoaded: false,
-//     }
-//   }
-//   render() {
-//       return (
-//           <SafeAreaView style={styles.container}>
-//               <MainTabNavigator />
-//           </SafeAreaView>
-//       );
-//   }
-// }
-//TailwindProvider
-//NavigatioContainer
-
-{/* <View style={styles.container}>
-      <Text>Je t'aime mon bebou! </Text>
-      <StatusBar style="auto" />
-    </View> */}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- StarNative/src/components/HomeScreen.js
+++ StarNative/src/components/HomeScreen.js
@@ -1,13 +1,13 @@
 import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import DetailsScreen from "./DetailsScreen";
 
 export default function HomeScreen({ navigation }) {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Text>Home Screenee</Text>
+              <Text>Home Screen</Text>
               <Button
                 title="Go to Details"
-                onPress={() => navigation.navigate('Details')}
+                onPress={() => navigation.navigate(DetailsScreen)}
                 />
             </View>
           );
