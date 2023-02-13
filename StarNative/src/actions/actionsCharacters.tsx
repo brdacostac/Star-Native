d6999f3e722111fe696278c6bd667bb156fb52ab
Noan07 <84435602+Noan07@users.noreply.github.com>
2023-02-13 09:31:46 +0100

    Début de mise en forme du detail

--- StarNative/src/actions/actionsCharacters.tsx
+++ StarNative/src/actions/actionsCharacters.tsx
@@ -8,11 +8,68 @@ export const loadCharactersSuccess = (characters : string[]) => {
 export const loadCharacters = () => async dispatch => {
     console.log("test5");
     try {
-        const charactersPromise = await fetch('https://akabab.github.io/starwars-api/api/all.json');
-        const characters = await charactersPromise.json();
-        dispatch(loadCharactersSuccess(characters));
+      const charactersPromise = await fetch('https://akabab.github.io/starwars-api/api/all.json');
+      const characters = await charactersPromise.json();
+      const modifiedCharactersMaster = characters.map(character => {
+        if (Array.isArray(character.masters)) {
+          return {
+            ...character,
+            masters: character.masters.map(subCharacter => {
+              if (typeof subCharacter === 'string') {
+                return subCharacter.replace(/ *\([^)]*\) */g, "");
+              }
+              return subCharacter;
+            })
+          };
+        } else if (typeof character.masters === 'string') {
+          return {
+            ...character,
+            masters: character.masters.replace(/ *\([^)]*\) */g, "")
+          };
+        }
+        return character;
+      });
+      const modifiedCharactersApprentices = modifiedCharactersMaster.map(character => {
+        if (Array.isArray(character.apprentices)) {
+          return {
+            ...character,
+            apprentices: character.apprentices.map(subCharacter => {
+              if (typeof subCharacter === 'string') {
+                return subCharacter.replace(/ *\([^)]*\) */g, "");
+              }
+              return subCharacter;
+            })
+          };
+        } else if (typeof character.apprentices === 'string') {
+          return {
+            ...character,
+            apprentices: character.apprentices.replace(/ *\([^)]*\) */g, "")
+          };
+        }
+        return character;
+      });
+      console.log(modifiedCharactersApprentices);
+      dispatch(loadCharactersSuccess(modifiedCharactersApprentices));
+
     } catch (error) {
         console.error('Error while fetching characters:', error);
     }
 };
   
+
+// try {
+//   const charactersPromise = await fetch('https://akabab.github.io/starwars-api/api/all.json');
+//   const characters = await charactersPromise.json();
+//   const modifiedCharacters = characters.map(character => {
+//       const masters = Array.isArray(character.masters)
+//       ? character.masters.map(subCharacter => typeof subCharacter === 'string' ? subCharacter.replace(/ *[)]∗[)]∗ */g, "") : subCharacter)
+//       : typeof character.masters === 'string' ? character.masters.replace(/ *[)]∗[)]∗ */g, "") : character.masters;
+//     const apprentices = Array.isArray(character.apprentices)
+//       ? character.apprentices.map(subCharacter => typeof subCharacter === 'string' ? subCharacter.replace(/ *[)]∗[)]∗ */g, "") : subCharacter)
+//     : typeof character.apprentices === 'string' ? character.apprentices.replace(/ *[)]∗[)]∗ */g, "") : character.apprentices;
+//   return {
+//     ...character,
+//     masters,
+//     apprentices
+//   };
+// });
--- StarNative/src/screens/DetailCharacterScreen.tsx
+++ StarNative/src/screens/DetailCharacterScreen.tsx
@@ -1,15 +1,19 @@
-import { View, Text, FlatList, Image, TouchableOpacity, Linking, Button } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity, Linking, Button, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useDispatch, useSelector } from 'react-redux';
 import { ADD_FAVORITE_CHARACTER, DELETE_FAVORITE_CHARACTER } from '../store/constantes';
+import { Ionicons } from '@expo/vector-icons';
+import { Animated, Easing } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const KEY_FAVORITE_CHARACTERS = 'favorite_characters';
 
 export default function DetailCharacterScreen({route}) {
   const character = route.params.character;
   const favoriteCharacters = useSelector(state => state.favoritesReducer.favoriteCharacters);
+  const dataCharactersState = useSelector((state) => state.charactersReducer.characters);
   const [isLiked, setIsLiked] = useState(false);
   const navigation = useNavigation();
   const dispatch = useDispatch();
@@ -19,9 +23,27 @@ export default function DetailCharacterScreen({route}) {
     const favoriteCharacter = favoriteCharacters.find(
       (favChar) => favChar.name === character.name
     );
-    
+
     setIsLiked(favoriteCharacter !== undefined);
   }, [favoriteCharacters, character]);
+
+  const [scaleValue, setScaleValue] = useState(new Animated.Value(1));
+  
+  const handleImagePressIn = () => {
+    Animated.timing(scaleValue, {
+      toValue: 1.3,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  };
+  
+  const handleImagePressOut = () => {
+    Animated.timing(scaleValue, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  };
 
   
   const addToFavorites = () => {
@@ -37,60 +59,125 @@ export default function DetailCharacterScreen({route}) {
         dispatch({ type: DELETE_FAVORITE_CHARACTER, payload: character });
       }
   };
+  
+  const formatWord = (word) => {
+    word = word.trim();
+    return word.charAt(0).toUpperCase() + word.slice(1);
+    };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', padding: 20 }}>
-      <Button
-        title="Go back"
-        onPress={() => navigation.goBack()}
-        buttonStyle={{ marginBottom: 20 }}
-      />
-      <Image
-        source={{ uri: character.image }}
-        style={{
-          width: 100,
-          height: 100,
-          borderRadius: 50,
-          overflow: 'hidden',
-          marginBottom: 20
-        }}
-      />
-      <Text style={{ fontWeight: 'bold', marginBottom: 10 }}>{character.name}</Text>
-      <Text style={{ marginBottom: 10 }}>{character.homeworld}</Text>
-      <Text style={{ marginBottom: 10 }}>Height: {character.height}</Text>
-      <Text style={{ marginBottom: 10 }}>Mass: {character.mass}</Text>
-      <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
-        <Text style={{ fontWeight: 'bold' }}>Masters:</Text>
-        <FlatList
-          horizontal={true}
-          data={Array.isArray(character.masters) ? character.masters : [character.masters]}
-          renderItem={({ item }) => <Text
-          style={{ marginLeft: 10 }}>{item}</Text>}
-          keyExtractor={(item, index) => index.toString()}
-          />
-          </View>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text style={{ fontWeight: 'bold' }}>Apprentices:</Text>
-          <FlatList
-          horizontal={true}
-          data={Array.isArray(character.apprentices) ? character.apprentices : [character.apprentices]}
-          renderItem={({ item }) => <Text
-          style={{ marginLeft: 10 }}>{item}</Text>}
-          keyExtractor={(item, index) => index.toString()}
-          />
-          </View>
-          <TouchableOpacity
-          style={{ marginTop: 20 }}
-          onPress={() => {
-          isLiked ? deleteFromFavorites() : addToFavorites();
-          }}
-          >
-          <Icon
-          name={isLiked ? 'heart' : 'heart-o'}
-          size={30}
-          color={isLiked ? 'red' : 'black'}
-          />
-          </TouchableOpacity>
+          <View style={{   padding: 25 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center'}}>
+              <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                style={{ position: 'absolute', left: 0 }}>
+                <Ionicons name="ios-arrow-back" size={30} color="#333" />
+              </TouchableOpacity> 
+              <Text style={{ fontWeight: 'bold', fontSize: 30, textAlign: 'center' }}>{character.name}</Text>
+            </View>
+
+            <ScrollView>
+
+            <TouchableWithoutFeedback
+              onPressIn={handleImagePressIn}
+              onPressOut={handleImagePressOut}
+            >
+              <Animated.Image
+                source={{ uri: character.image }}
+                style={{
+                  width: 200,
+                  height: 200,
+                  borderRadius: 50,
+                  overflow: 'hidden',
+                  alignSelf: 'center',
+                  margin: 20,
+                  transform: [{ scale: scaleValue }]
+                }}
+              />
+            </TouchableWithoutFeedback>
+            <View style={{ flexDirection: 'row', paddingBottom: 7, paddingTop: 20 }}>
+              <Text style={styles.label}>GENDER</Text>
+              <Text style={styles.value} >{formatWord(character.gender)}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', paddingBottom: 7 }}>
+              <Text style={styles.label}>HOMEWORLD</Text>
+              <Text style={styles.value} >{character.homeworld ? formatWord(character.homeworld) : "Aucun"}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', paddingBottom: 7 }}>
+              <Text style={styles.label}>HEIGHT</Text>
+              <Text style={styles.value} >{character.height ? character.height + " meters" : "Inconnue"}</Text>
+            </View>
+            <View style={{ flexDirection: 'row' , paddingBottom: 7 }}>
+              <Text style={styles.label}>MASS</Text>
+              <Text style={styles.value} >{character.mass? character.mass + " kilograms" : "Inconnue"}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', paddingBottom: 7 }}>
+              <Text style={styles.label}>SPECIES</Text>
+              <Text style={styles.value} >{formatWord(character.species)}</Text>
+            </View>
+            <TouchableOpacity onPress={() => Linking.openURL(character.wiki)}>
+              <Text style={{ color: 'blue' }}>Learn more on Wikipedia</Text>
+            </TouchableOpacity>
+            {character.masters && (dataCharactersState.filter(favChar => character.masters.includes(favChar.name)).length > 0 || dataCharactersState.find(favChar => favChar.name === character.masters)) &&
+              <View style={{ flexDirection: 'column', alignItems: 'flex-start', marginBottom: 20 }}>
+                <Text style={{ fontWeight: 'bold' }}>Masters:</Text>
+                <FlatList
+                  horizontal={true}
+                  data={
+                    Array.isArray(character.masters) ?
+                      dataCharactersState.filter(favChar => character.masters.includes(favChar.name)) :
+                        [dataCharactersState.find(favChar => favChar.name === character.masters)] 
+                  }
+                    renderItem={({ item }) => (
+                      <TouchableOpacity style={{ marginLeft: 10 }}
+                                        onPress={() => navigation.navigate('CharacterDetails', { character: item })}>
+                        {/* <Text>{item.name}</Text> */}
+                        <Image source={{ uri: item.image }} style={{ width: 65, height: 65, borderRadius: 50 , borderWidth: 1, borderColor: '#333' }} />
+                      </TouchableOpacity>)}
+                    keyExtractor={(item, index) => index.toString()} />
+              </View>}
+            
+            {character.apprentices  && (dataCharactersState.filter(favChar => character.apprentices.includes(favChar.name)).length > 0 || dataCharactersState.find(favChar => favChar.name === character.apprentices)) &&
+              <View style={{ flexDirection: 'column', alignItems: 'flex-start'}}> 
+                  <Text style={{ fontWeight: 'bold' }}>Apprentis:</Text>
+                  <FlatList
+                    horizontal={true}
+                    data={Array.isArray(character.apprentices) ?
+                      dataCharactersState.filter(favChar => character.apprentices.includes(favChar.name)) :
+                        [dataCharactersState.find(favChar => favChar.name === character.apprentices)] }
+                    renderItem={({ item }) => (
+                      <TouchableOpacity style={{ marginLeft: 10 }}
+                                        onPress={() => navigation.navigate('CharacterDetails', { character: item })}>
+                        {/* <Text>{item.name}</Text> */}
+                        <Image source={{ uri: item.image }} style={{ width: 65, height: 65, borderRadius: 50 }} />
+                      </TouchableOpacity>)}
+                    keyExtractor={(item, index) => index.toString()} />
+              </View>}
+            <TouchableOpacity
+              style={{ marginTop: 20 }}
+              onPress={() => {
+              isLiked ? deleteFromFavorites() : addToFavorites();
+              }}>
+              <Icon
+                name={isLiked ? 'heart' : 'heart-o'}
+                size={30}
+                color={isLiked ? 'red' : 'black'}
+              />
+            </TouchableOpacity>
+            </ScrollView>
           </View>
           );
         }
+
+
+const styles = StyleSheet.create({
+  label: {
+    marginBottom: 10,
+    fontSize: 20,
+    width: '35%',
+  },
+  value: {
+    marginLeft: '20%',
+    fontSize: 20,
+  },
+});
