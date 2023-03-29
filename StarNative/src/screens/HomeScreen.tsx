d42889f73ba8d827a64a1112fb9b4d3c48c27c21
brdacostac <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-03-29 08:54:31 +0200

    Idee image viewer abandonné, la librarie est pourri et accept juste des uri.

--- StarNative/src/screens/HomeScreen.tsx
+++ StarNative/src/screens/HomeScreen.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {View, Text, Image, Animated, StyleSheet, ScrollView,Dimensions} from 'react-native';
 import {useTheme} from "../context/theme-context";
-import { Image360viewer } from 'image360viewer';
 
 export default function HomeScreen() {
     const [fadeAnim] = useState(new Animated.Value(0));
@@ -16,9 +15,8 @@ export default function HomeScreen() {
     }, []);
 
 
-    return (
-
-        <ScrollView style={styles.container}>
+  return (
+    <ScrollView style={styles.container}>
         <View style={styles.container}>
             <Animated.View
                 style={{
@@ -43,49 +41,6 @@ export default function HomeScreen() {
                 planets, our app has got you covered. So why wait? Download Star Native
                 today and start exploring the vast world of Star Wars!
             </Text>
-        </View>
-        </ScrollView>
-    );
-
-  return (
-    <ScrollView style={styles.container}>
-      <View style={styles.container}>
-        <Animated.View
-          style={{
-            ...styles.title,
-            opacity: fadeAnim,
-          }}
-        >
-          <Image
-            source={require('../../assets/img/logoLightSide.png')}
-            style={styles.logo}
-          />
-        </Animated.View>
-        <Text style={styles.title}>
-          Welcome to Star Native, the ultimate Star Wars encyclopedia!
-        </Text>
-        <Text style={styles.text}>
-          This application is your go-to source for all information related to
-          the Star Wars universe. Whether you're a die-hard fan or just starting
-          to explore the world of Star Wars, our app has everything you need to
-          know about your favorite characters, planets, and more. Built by
-          developers Bruno Cunha and Noan Randon, Star Native uses a Web API to
-          bring you the most up-to-date information about the Star Wars universe.
-          From information of your favorite characters to a comprehensive overview
-          of the different planets, our app has got you covered. So why wait?
-          Download Star Native today and start exploring the vast world of Star
-          Wars!
-        </Text>
-
-
-          <Image360viewer
-              images={[
-                  require('../../assets/VaderImages/Vader-1_0000_Camada-22.png'),
-                  require('../../assets/VaderImages/Vader-1_0002_Camada-20.png'),
-              ]}
-              width={300}
-          />}
-
       </View>
     </ScrollView>
   );
